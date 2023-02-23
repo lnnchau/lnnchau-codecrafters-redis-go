2663b7728d4c14266a3475cf59b07d435b327b0c
Chau Le <nganchau.lenguyen@gmail.com>
2023-02-23 09:38:31 +0700

    handle multiple pings in one connection

--- app/server.go
+++ app/server.go
@@ -8,6 +8,19 @@ import (
 	"os"
 )
 
+func processConn(conn net.Conn) {
+	buf := make([]byte, 1024)
+	length, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("error reading from client: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Message received:\n%s\n", string(buf[:length]))
+
+	conn.Write([]byte("+PONG\r\n"))
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -19,12 +32,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+	defer conn.Close()
+	for {
+		processConn(conn)
+	}
 }
