f5b391b71e4b99f7f3eaeda8da02c7bead79ba4a
Chau Le <nganchau.lenguyen@gmail.com>
2023-02-24 11:00:39 +0700

    handle multiple clients

--- app/server.go
+++ app/server.go
@@ -21,6 +21,13 @@ func processConn(conn net.Conn) {
 	conn.Write([]byte("+PONG\r\n"))
 }
 
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		processConn(conn)
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -33,14 +40,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	defer conn.Close()
 	for {
-		processConn(conn)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+	
+		go handleConn(conn)
 	}
 }
